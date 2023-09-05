6c3597a0f7c52ccf24de9474f804af514554612d
Isadora Hoelscher <isadorahoelscher@gmail.com>
2023-09-05 01:12:34 -0300

    add queryclient and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./routes";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: false,
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Router />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 }
 
